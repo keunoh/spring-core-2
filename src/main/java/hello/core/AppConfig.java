4257bb724695d67eabe8d1bdefa9992593f8711a
Keunoh <rmsdhcjswo1@gmail.com>
2023-02-27 23:14:10 +0900

    리팩토링 -> new 생성자를 메서드로 분리

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,5 +1,6 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -8,12 +9,26 @@ import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
+    //리팩토링 -> 역할과 구현 클래스가 한눈에 들어오도록 했다.
+    //기존에는 new 생성자를 이용해서 의존성 주입을 해주었는데
+    //리팩토링을 이용, 메서드로 분리함으로써 역할과 구현이
+    //한눈에 들어오게 함이 그 목적이다.
+    //애플리케이션 전체 구성이 어떻게 되어있는지
+    //빠르게 파악할 수 있게된 것이다.
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
